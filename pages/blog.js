aafc6d660acd1f5ec2ee854238bc8de868c438e0
arabjon988 <arabkhon3133@gamil.com>
2022-06-22 20:51:36 +0500

    add js files

---
+++ pages/blog.js
@@ -0,0 +1,13 @@
+// import stylebg from '../styles/Blog.module.css'
+// style={stylebg.salom}
+
+
+function blog  (){
+  return (
+    <div>
+        <h1  >salom dunyo</h1>
+    </div>
+  )
+}
+
+export default blog;
---
+++ pages/test/post.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+
+function post (){
+    return (
+      <div>
+        <Link href='/' >
+          <p>home</p>
+        </Link>
+          <h1  >salom dunyo</h1>
+          <h1  >salom dunyo</h1>
+          <h1  >salom dunyo</h1>
+
+      </div>
+    )
+  }
+  
+  export default post;
